c314c268d98c4b724907416759d7717133128a99
Manuel Geyer <manuelgeyer@web.de>
2017-11-03 12:59:11 +0100

    Testng init

---
+++ src/test/java/org/gradle/rbbTest2.java
@@ -0,0 +1,17 @@
+package org.gradle;
+
+import org.testng.annotations.Test;
+
+public class rbbTest2 {
+	@Test
+	public void firstTestCase()
+	{
+		System.out.println("im in first test case from demo2 Class");
+	}
+	
+	@Test
+	public void secondTestCase()
+	{
+		System.out.println("im in second test case from demo2 Class");
+	}
+}
