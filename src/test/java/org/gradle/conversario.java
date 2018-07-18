d15b7b176c8d90e3b998dc7f8cfd8a7c188991ec
Manuel Geyer <manuelgeyer@web.de>
2018-07-18 10:56:36 +0200

    introducing Goodboy and Badboy comments

--- src/test/java/org/gradle/conversario.java
+++ src/test/java/org/gradle/conversario.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -34,23 +35,25 @@ public class conversario {
 	private String baseUrl = "https://www.rbb-online.de/test/abnahme/kommentarfunktion/kommentare-eingeschaltet/mge/conversation.htm/writecomment=true.html#new_comment_box";
 
 	
+	
 	@BeforeTest
 	public void launchBrowser() {
 		
 		/* Chrome Browser */
-		System.setProperty("webdriver.chrome.driver", driverPath);
+		/*System.setProperty("webdriver.chrome.driver", driverPath);
 		driver = new ChromeDriver(); 
 		driver.get(baseUrl);	
-		
+		*/
 		/* Headless Chrome Browser */
-		/*System.setProperty("webdriver.chrome.driver", driverPath);
+		System.setProperty("webdriver.chrome.driver", driverPath);
 		// Add options to Google Chrome. The window-size is important for responsive sites
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("headless");
 		options.addArguments("window-size=1200x600");
 		driver = new ChromeDriver(options);
-		driver.get(baseUrl);*/
+		driver.get(baseUrl);
 	}
+	
 	
 	
 	/* priority 0    	(lowest, is going to be executed first)
@@ -58,77 +61,57 @@ public class conversario {
 	 * */
 	@Test(priority = 0)
 	public void sendNiceComment() { 
-		
-
+				
 		JavascriptExecutor js = (JavascriptExecutor)driver;
 		String sText =  js.executeScript("return document.documentElement.innerText;").toString();
+		//System.out.println("AAA" + sText);
 		
-		System.out.println("AAA" + sText);
-		
+		/* Fill name into form */
 		driver.findElement(By.id("mailpar_1")).click();
-		try {
-			Thread.sleep(800);
-		} catch (InterruptedException e) { 
-			e.printStackTrace();
-		}/*
-		driver.findElement(By.id("mailpar_1")).sendKeys("AAA");
-		try {
-			Thread.sleep(800);
-		} catch (InterruptedException e) { 
-			e.printStackTrace();
-		}*/
-		(new Actions(driver)).sendKeys("AAA").perform();
+		//driver.findElement(By.id("mailpar_1")).sendKeys("RBB_Goodboy");
+		(new Actions(driver)).sendKeys("RBB_Goodboy").perform();
+		
+		/* Fill e-mail into form */
+		driver.findElement(By.id("mailpar_2")).click();
+		(new Actions(driver)).sendKeys("manuelgeyer@web.de").perform();
+		
+		/* Fill comment into form */
+		driver.findElement(By.id("mailpar_4")).click();
+		(new Actions(driver)).sendKeys("NiceComment: Bitte ein Bild von der heiligen Magdalena (vor-, nach-, während der Sünde?).").perform();
+		
 
-		
-		
-		//System.out.println("AAA" + driver.findElement(By.id("comment_form")).findElements(By.className("type-text")).toString());
-	
-		//By.xpath("//td[contains(text(),'Ort')]/following-sibling::td//input[@type='text']");
-
-		
-		//js.executeScript("document.getElementById('Ort')");
-		//.setAttribute('value', 'new value for element')
-		
-		
-		//WebElement kommentarButton = driver.findElement(By.id("new_comment_link"));
-		
-		/*driver.findElement(By.id("new_comment_link")).click();
-		try {
-			Thread.sleep(1400);
-		} catch (InterruptedException e) { 
-			e.printStackTrace();
-		}*/
-
-		
-		/*By hiddenObj = By.xpath("//*[@id='mailpar_1' and @type='hidden']");
-		hiddenObj.findElement(driver.findElement(By.id("mailpar_1"))).sendKeys("GoodBoy");
-		try {
-			Thread.sleep(700);
-		} catch (InterruptedException e) { 
-			e.printStackTrace();
-		}*/
-		
-		//WebElement elem = driver.findElement(By.xpath("//*[@class='js_toggle'"));
-		//String js = "arguments[0].style.height='auto'; arguments[0].style.visibility='visible';";
- 
-		//((JavascriptExecutor) driver).executeScript(js, elem);
-		
-		
-		try {
-			Thread.sleep(7000);
-		} catch (InterruptedException e) { 
-			e.printStackTrace();
-		}/*
-		//driver.findElement(By.id("mailpar_1")).sendKeys("GoodBoy");
-		try {
-			Thread.sleep(700);
-		} catch (InterruptedException e) { 
-			e.printStackTrace();
-		}*/
-		
+		driver.findElement(By.xpath("//button[@value='Abschicken'][@name='savecomment'][@type='submit']")).click();
 		
 		AssertJUnit.assertEquals("AAA", "AAA");
-
+	}  
+	
+	
+	
+	@Test(priority = 1)
+	public void sendBadComment() { 
+		
+		driver.get(baseUrl);	
+		
+		/* Fill name into form */
+		driver.findElement(By.id("mailpar_1")).click();
+		(new Actions(driver)).sendKeys("RBB_Badboy").perform();
+		
+		/* Fill e-mail into form */
+		driver.findElement(By.id("mailpar_2")).click();
+		(new Actions(driver)).sendKeys("manuelgeyer@web.de").perform();
+		
+		/* Fill comment into form */
+		driver.findElement(By.id("mailpar_4")).click();
+		(new Actions(driver)).sendKeys("BadComment: Das ist ein bescheuerter Kommentar gegen den Rundfunk. Mit Schimpfwörtern wie damisch depp depperter oder Doldi mit einer Drudschen an der Spitzen. Blöder Scheiß funktioniert wieder net.").perform();
+		
+		driver.findElement(By.xpath("//button[@value='Abschicken'][@name='savecomment'][@type='submit']")).click();
+		
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) { 
+			e.printStackTrace();
+		}
+		AssertJUnit.assertEquals("AAA", "AAA");
 	}  
 	
 	
