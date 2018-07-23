768d34559da0bda1c95bb8d496643be8d5cd00fb
Manuel Geyer <manuelgeyer@web.de>
2018-07-23 14:18:36 +0200

    Add checkComment module

--- src/test/java/org/gradle/conversario.java
+++ src/test/java/org/gradle/conversario.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
